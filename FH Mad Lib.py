e49eaed5f499ebbfe460b12c44e5ff291ef3a733
FinnFH <33524217+FinnFH@users.noreply.github.com>
2017-11-09 12:07:12 -0500

    initial

---
+++ FH Mad Lib.py
@@ -0,0 +1,46 @@
+print("Write a name")
+name1 = input()
+
+print("Write if the name is a he or she")
+gender1 = input()
+
+print("Write another name")
+name2 = input()
+
+print("Write another name")
+name3 = input()
+
+print("Write a noun")
+noun1 = input()
+
+print("Write another noun")
+noun2 = input()
+
+print("Write another noun")
+noun3 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+print("Write another adjective")
+adjective2 = input()
+
+print("Write a verb")
+verb1 = input()
+
+print("Write another verb")
+verb2 = input()
+
+print("Write a verb you can do to someone else. Example: attack")
+verb3 = input()
+
+print("Write a day of the week")
+weekday1 = input().title()
+
+print("Write a country")
+country1 = input().title
+
+### MAD LIB ###
+
+print("After " + name1 + " " + verb1 +" the " + adjective1 + " " + noun1 + ", " + gender1 + " decided to " + verb2 + " " + name2 + "." " Then " + gender1 +
+      " took the " + adjective2 + noun3" of " + name3 + " which " + name1 + " had " + verb3 + " on " + weekday1 + " and sent it to " + country1 + ".")
